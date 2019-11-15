6ce73a847170a2e32276bc74b1f464c0a9b27493
Valky <its.valky@outlook.com>
2019-11-16 00:14:50 +0100

    added_closet

---
+++ scripts/closet.js
@@ -0,0 +1,21 @@
+
+// preview clothes
+$("input[name='clothes']").change(function(){
+    
+    var cloth = $('input[type=radio][name=clothes]:checked').prop('id');
+    var model = cloth.replace("cloth_", "");
+
+    $("#valky-clothes").attr("src", "/img/model/closet/" + model + ".png");
+
+});
+
+
+// start web render
+$("input[name='submit']").on("click", function(){
+    
+    var cloth = $('input[type=radio][name=clothes]:checked').prop('id');
+    var model = cloth.replace("cloth_", "");
+
+    window.open("/webgl?model=" + model, "_self");
+
+});
