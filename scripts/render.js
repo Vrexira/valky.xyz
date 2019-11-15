577bf63926aba466442c23d4a7b019fbf0202e45
Valky <its.valky@outlook.com>
2019-11-15 22:08:24 +0100

    webgl_ready

---
+++ scripts/render.js
@@ -0,0 +1,23 @@
+// get url params
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1];
+        }
+
+        if (sParameterName[1] === undefined) {
+            window.open("/404","_self");
+        }
+    }
+};
+
+var model = getUrlParameter('model');
+
+setTimeout(function(){ $("#glb_clothes").attr("src", "/obj/" + model + ".glb"); }, 50);
