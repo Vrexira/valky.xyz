fdad68e439a7365a1093758974e053914d91a345
Valky <its.valky@outlook.com>
2019-11-17 15:43:36 +0100

    navmesh

--- scripts/movement.js
+++ scripts/movement.js
@@ -2,14 +2,12 @@
 // shift - move faster
 $(document).keydown(function (e) {
     if (e.keyCode == 16) {
-        console.log("Pressed");
         $("#world").attr("wasd-controls", "acceleration: 5000");
     }
 });
 
 $(document).keyup(function (e) {
     if (e.keyCode == 16) {
-        console.log("Not Pressed");
         $("#world").attr("wasd-controls", "acceleration: 600");
     }
 });
