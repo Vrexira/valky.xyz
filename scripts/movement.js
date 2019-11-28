5596b40174a6a450a2d43234cda8b9b0e90247ac
Valky <53298849+Its-Valky@users.noreply.github.com>
2019-11-28 18:43:38 +0100

    update
    - player movement

--- scripts/movement.js
+++ scripts/movement.js
@@ -2,12 +2,12 @@
 // shift - move faster
 $(document).keydown(function (e) {
     if (e.keyCode == 16) {
-        $("#world").attr("wasd-controls", "acceleration: 5000");
+        $("#valky_player_char").attr("wasd-controls", "acceleration: 5000");
     }
 });
 
 $(document).keyup(function (e) {
     if (e.keyCode == 16) {
-        $("#world").attr("wasd-controls", "acceleration: 600");
+        $("#valky_player_char").attr("wasd-controls", "acceleration: 600");
     }
 });
