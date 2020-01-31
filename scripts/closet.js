e87251195db6e80da156d8c15ff77234837fa0b4
Valky <its.valky@outlook.com>
2020-01-31 14:21:53 +0100

    valk-ai-start

--- scripts/closet.js
+++ scripts/closet.js
@@ -16,6 +16,6 @@ $("input[name='submit']").on("click", function(){
     var cloth = $('input[type=radio][name=clothes]:checked').prop('id');
     var model = cloth.replace("cloth_", "");
 
-    window.open("/webgl?model=" + model, "_self");
+    window.open("/valkyria/webgl?model=" + model, "_self");
 
 });
