0591cae05b1839c924f90a45a593a61d602f84f0
Valky <its.valky@outlook.com>
2020-01-31 16:56:59 +0100

    update
    - seperate special outfits
    - lower chances for special outfits

--- scripts/renderai.js
+++ scripts/renderai.js
@@ -1,5 +1,6 @@
 // get url params
 var getUrlParameter = function getUrlParameter(sParam) {
+
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
@@ -8,25 +9,44 @@ var getUrlParameter = function getUrlParameter(sParam) {
     for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
 
+
+        // outfit given by url
         if (sParameterName[0] === sParam) {
+            
             return sParameterName[1];
         }
 
+
+        // outfit given random
         if (sParameterName[1] === undefined) {
             
+            // normal outifts array
+            // chance: 5.263157894736842
             var modelOutfits = [
                 'aot', 'armor', 'bandage', 
-                'college', 'goddess', 'hunter', 
-                'kitty', 'lingerie', 'mecha', 
+                'college', 'mecha', 'hunter', 
                 'metal', 'party', 'photon', 
                 'police', 'pubg', 'santa', 
                 'sheriff', 'summer', 'winter'
             ]; 
+            
+            // special outift array
+            // chance: 1.754385964912281
+            var modelOutfitsSpecial = [
+                'goddess', 'kitty', 'lingerie'
+            ];
 
-            var rParameterName = modelOutfits[(Math.random() * modelOutfits.length) | 0]
+
+            // add special outfit to normal
+            var rParameterSpecial = modelOutfitsSpecial[(Math.random() * modelOutfitsSpecial.length) | 0];
+            modelOutfits.push(rParameterSpecial);
+
+            // choose displayed outfit
+            var rParameterName = modelOutfits[(Math.random() * modelOutfits.length) | 0];
+            
             
             return rParameterName;
-            
+
         }
 
     }
