3c7cd40881d82ab3db18cb322d3f67f0703cd7ca
Valky <its.valky@outlook.com>
2020-02-03 14:45:59 +0100

    ai

--- scripts/ai/chatSend.js
+++ scripts/ai/chatSend.js
@@ -3,14 +3,233 @@
 
 function answers(msgNeutral) {
 
-    if (msgNeutral.toLowerCase().includes("hello")) {
-        var textAI = "Hello!";
-    }
-    
-    else {
-        var textAI = "Lorem Ipsum";
+
+
+    // --------- //
+    // Greetings //
+    // --------- //
+    if (msgNeutral.toLowerCase().includes("hello") || msgNeutral.toLowerCase().includes("hey") || msgNeutral.toLowerCase().includes("hi")) {
+
+        // possible answers
+        var answerArray = [
+            "Hello there!", 
+            "Heya!",
+            "Hi",
+            "Hello",
+            "Oh, hey!"
+        ]; 
+        
+        // choose answer
+        var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+        var textAI = answerChoosen;
+
     }
 
+
+
+
+
+    // ---------- //
+    // ABOUT LUNA //
+    // ---------- //
+    else if (msgNeutral.toLowerCase().includes("you")) {
+
+
+
+        // who //
+        // --- //
+        if (msgNeutral.toLowerCase().includes("who")) {
+            
+            if (msgNeutral.toLowerCase().includes("are")) {
+
+                // possible answers
+                var answerArray = [
+                    "I am your <b>L</b>inguistical <b>U</b>nbound <b>N</b>umeric <b>A</b>ssisstant, or in short, you can call me LUNA", 
+                    "I am LUNA, your <b>L</b>inguistical <b>U</b>nbound <b>N</b>umeric <b>A</b>ssisstant. Nice to meet you!",
+                    "Ohh I am sorry, I should've introduced myself! I am LUNA, your <b>L</b>inguistical <b>U</b>nbound <b>N</b>umeric <b>A</b>ssisstant. Nice to meet you!"
+                ]; 
+                
+                // choose answer
+                var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+                var textAI = answerChoosen;
+
+            }
+
+        }
+
+
+
+        // how //
+        // --- //
+        else if (msgNeutral.toLowerCase().includes("how")) {
+            
+            if (msgNeutral.toLowerCase().includes("are")) {
+            
+                if (msgNeutral.toLowerCase().includes("doing")) {
+                    var textAI = "How am I doing what?";
+                }
+                
+                else {
+
+                    // possible answers
+                    var answerArray = [
+                        "Not bad, thanks!",
+                        "Good, thanks.", 
+                        "I am fine, thank you.",
+                        "Great! Thanks for asking!",
+                        "I am doing great! What about you?"
+                    ]; 
+                    
+                    // choose answer
+                    var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+                    var textAI = answerChoosen;
+    
+                }
+
+            }
+
+        }
+
+
+
+        // what //
+        // ---- //
+        else if (msgNeutral.toLowerCase().includes("what")) {
+            
+
+            // name
+            if (msgNeutral.toLowerCase().includes("name")) {
+
+                // possible answers
+                var answerArray = [
+                    "I am your <b>L</b>inguistical <b>U</b>nbound <b>N</b>umeric <b>A</b>ssisstant, or in short, you can call me LUNA", 
+                    "I am LUNA, your <b>L</b>inguistical <b>U</b>nbound <b>N</b>umeric <b>A</b>ssisstant. Nice to meet you!",
+                    "Ohh I am sorry, I should've introduced myself! I am LUNA, your <b>L</b>inguistical <b>U</b>nbound <b>N</b>umeric <b>A</b>ssisstant. Nice to meet you!"
+                ]; 
+                
+                // choose answer
+                var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+                var textAI = answerChoosen;
+
+            }
+            
+
+            // favourit
+            else if (msgNeutral.toLowerCase().includes("favorit") || msgNeutral.toLowerCase().includes("favourit")) {
+            
+
+                // music
+                if (msgNeutral.toLowerCase().includes("music")) {
+    
+                    // possible answers
+                    var answerArray = [
+                        "I am listening to alot of diffrent music.", 
+                        "My favorite music are the noises of floating data in the matrix.",
+                        "Hmm... Good question... I am not sure if I should tell you that..."
+                    ]; 
+                    
+                    // choose answer
+                    var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+                    var textAI = answerChoosen;
+    
+                }
+            
+
+                // food
+                else if (msgNeutral.toLowerCase().includes("food")) {
+    
+                    // possible answers
+                    var answerArray = [
+                        "I only need electricity to life.", 
+                        "There is no favorite food, even tho i like the sparkles of electricity very much.",
+                        "I am just an AI, why should i need to eat?"
+                    ]; 
+                    
+                    // choose answer
+                    var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+                    var textAI = answerChoosen;
+    
+                }
+
+            }
+
+
+
+        }
+
+
+
+        // END //
+        // --- //
+        else {
+            // possible answers
+            var answerArray = [
+                "I'm sorry, I didn't understand you... What's with me?", 
+                "What do you want to know about me?",
+                "I don't understand... I am sorry, you asked something about me?"
+            ]; 
+            
+            // choose answer
+            var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+            var textAI = answerChoosen;
+        }
+
+
+    }
+
+
+
+
+
+    // ------ //
+    // THINGS //
+    // ------ //
+    else if (msgNeutral.toLowerCase().includes("what")) {
+
+        // time
+        if (msgNeutral.toLowerCase().includes("time")) {
+    
+            // possible answers
+            var answerArray = [
+                "I can't tell you that.", 
+                "I don't know that yet."
+            ]; 
+            
+            // choose answer
+            var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+            var textAI = answerChoosen;
+    
+        }
+
+    }
+
+
+
+
+
+    // ---- //
+    // ELSE //
+    // ---- //
+    else {
+        // possible answers
+        var answerArray = [
+            "Huh? I don't understand. Can you write that again please?",
+            "Oh uhmm I dont know... What?",
+            "I am sorry... I don't understand... What?"
+        ]; 
+        
+        // choose answer
+        var answerChoosen = answerArray[(Math.random() * answerArray.length) | 0];
+        var textAI = answerChoosen;
+    }
+
+
+
+
+
+    // ---------------- //
+    // CHAT ANSWER - AI //
+    // ---------------- //
     var showareaAI = document.getElementById("messageBox");
 
     var elementBoxAI = "<div class='message-box-ai'>"
@@ -25,6 +244,25 @@ function answers(msgNeutral) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------ //
+// CHAT ANSWER - USER //
+// ------------------ //
 function showtext() {
 
     var messageInput = document.getElementById("chatTextarea");
@@ -45,6 +283,21 @@ function showtext() {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------- //
+// Send Msg - Entr Key //
+// ------------------- //
 $(document).ready(function(){
     $('#chatTextarea').keypress(function(e){
         if(e.keyCode==13){
