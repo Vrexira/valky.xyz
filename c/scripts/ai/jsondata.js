833a557280afdacb060b27ab59ec25ccd3ca4ed2
Valky <its.valky@outlook.com>
2020-02-04 17:16:50 +0100

    update

--- c/scripts/ai/jsondata.js
+++ c/scripts/ai/jsondata.js
@@ -1,14 +1,46 @@
 
 if (Cookies.get('admin') == "1") {
 
+
+    // luna
     $.getJSON("/m/json/luna.json", function (jsonFromFile) {
 
         $(function () {
-            $('#table').bootstrapTable({
+            $('#table-luna').bootstrapTable({
                 data: jsonFromFile
             });
         });
 
     });
 
+
+    // welcome
+    $.getJSON("/m/json/welcome.json", function (jsonFromFile) {
+
+        $(function () {
+            $('#table-welcome').bootstrapTable({
+                data: jsonFromFile
+            });
+        });
+
+    });
+
+
+    // runic
+    $.getJSON("/m/json/runic.json", function (jsonFromFile) {
+
+        $(function () {
+            $('#table-runic').bootstrapTable({
+                data: jsonFromFile
+            });
+        });
+
+    });
+
+
+
+}
+
+else {
+    window.open("/v/404","_self");
 }
