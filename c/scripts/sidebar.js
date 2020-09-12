3ef697959baff26c3f48eb11454f99567be1d973
Valky <53298849+Its-Valky@users.noreply.github.com>
2020-09-12 15:57:34 +0200

    added sprites and sidebar

--- c/scripts/sidebar.js
+++ c/scripts/sidebar.js
@@ -1,8 +1,10 @@
 
 function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("sidenav").style.width = "300px";
+    document.getElementById("navbar").style.display = "none";
 }
 
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
+    document.getElementById("navbar").style.display = "";
 }
