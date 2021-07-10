96638a2c494a975ec7e08459d82223ebee7f6884
Valky <fischer@valkyteq.com>
2021-07-11 01:49:38 +0200

    Adjusted docstring

--- ValkFrame/dev/valkframe.dev.js
+++ ValkFrame/dev/valkframe.dev.js
@@ -1,8 +1,8 @@
-/**!
+/**
  * @fileOverview Simple library with different smol functions :3
  * @version 0.1.1
- * @license
- * Copyright (c) 2021 VALKYTEQ
+ * @license Copyright (c) 2021 VALKYTEQ
+ * @IMPORTANT Needs valkframe.dev.css added to the project
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,12 +21,15 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ * ----------------
+ * ----------------
  *
- * @IMPORTANT Needs valkframe.dev.css added to the project
- */
-
-
-/**
+ *
+ *
+ *
+ *
+ *
+ *
  * AMPLIFY MEDIA
  * ----------------
  * ----------------
